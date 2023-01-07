50fbd3fd6dd99e1a5957d892a83e03a0c50a3f70
shinymew <maxim.odenweller@thm.mni.de>
2023-01-07 13:52:50 +0100

    add offer page, missing methods

--- frontend/src/components/createOffer.vue
+++ frontend/src/components/createOffer.vue
@@ -1,11 +1,78 @@
 <template>
-    <v-container>
+    <v-container align="center">
         <!-- Angebot anlegen -->
-        <c-vard>
-            <v-card-title>
-                
+        <v-card rounded="4" style="max-width: 1300px">
+            <v-card-title justify="center" align="center">
+                Jetzt dein Angebot listen!
             </v-card-title>
-        </c-vard>
+        </v-card>
+
+        <v-card rounded="4" v-if="vehicles.length===0" class="my-8 content-card">
+            <v-card-text align="center">
+                Du hast leider noch keine Autos registriert. 
+                Unter <router-link to="/vehicles" style="text-decoration: none">Meine Fahrzeuge</router-link> kannst du dein erstes Auto anlegen.
+            </v-card-text>
+        </v-card>
+
+        <v-card rounded="4" class="my-8 content-card" v-if="vehicles.length>0">
+            <v-card-text>
+                <v-form class="login-form">
+                    <v-container>
+                        <v-select 
+                        label="Wähle dein Auto"
+                        :v-model="offer.vehicle">
+
+                        </v-select>
+                        <v-row>
+                            <v-col
+                            cols="12"
+                            sm="6"
+                            >
+                            <v-text-field
+                                v-model="offer.start"
+                                label="Abholort"
+                                type="text"
+                                required
+                            ></v-text-field>
+                            </v-col>
+
+                            <v-col
+                            cols="12"
+                            sm="6"
+                            >
+                            <v-text-field
+                                v-model="offer.end"
+                                label="Ankunftsort"
+                                type="text"
+                                required
+                            ></v-text-field>
+                            </v-col>
+                        </v-row>
+                        <v-textarea
+                        v-model="offer.description"
+                        label="Gib deinem Angebot eine Beschreibung!"
+                        counter
+                        :rules="rules"
+                        >
+                        </v-textarea>
+
+                        <v-row align="center">
+                            <v-col cols="12" sm="6">
+                                <v-text-field
+                                v-model="offer.price"
+                                label="Preis in Euro"
+                                type="number"
+                                required
+                                ></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6">
+                                <v-btn color="green" @click="createOffer()">Angebot machen</v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-form>
+            </v-card-text>
+        </v-card>
     </v-container>
 </template>
 
@@ -14,13 +81,34 @@ export default {
     name: "createOffer",
     data() {
         return {
-            tab: null
+            offer: {
+                description: "",
+                price: "",
+                vehicle: "",
+                start: "",
+                end: "",
+            },
+            vehicles: [],
+            rules: [v => v.length <= 128 || 'Max 128 characters, everything after will be cut off'],
         };
     },
+    methods: {
+        getVehicles() {
+            console.log("get vehicles");
+        },
+        createOffer() {
+            console.log(this.offer);
+        },
+    }
 };
 
 
 </script>
 
 <style scoped>
+.content-card {
+    max-width: 600px;
+    margin: 0 auto;
+}
+
 </style>
