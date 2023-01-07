e9f2e4f1ad59306ead6852886c1042a8a91e244e
shinymew <maxim.odenweller@thm.mni.de>
2023-01-07 20:59:25 +0100

    add createOffer, bugged method (pls fix)

--- frontend/src/components/createOffer.vue
+++ frontend/src/components/createOffer.vue
@@ -19,10 +19,10 @@
                 <v-form class="login-form">
                     <v-container>
                         <v-select 
-                        v-model="defaultCar"
-                        :items="cars"
-                        item-title="brand"
-                        item-value="model"
+                        v-model="currentCar"
+                        :items="carNames"
+                        item-title="carname"
+                        item-value="vehicleId"
                         label="Select"
                         return-object
                         single-line
@@ -47,7 +47,7 @@
                             sm="6"
                             >
                             <v-text-field
-                                v-model="offer.end"
+                                v-model="offer.destination"
                                 label="Ankunftsort"
                                 type="text"
                                 required
@@ -87,6 +87,7 @@ export default {
     name: "createOffer",
     beforeMount() {
         this.getCars()
+        this.checkLogin()
     },
     created() {
         document.title = "Angebot anlegen";
@@ -97,15 +98,18 @@ export default {
             url: 'http://localhost:3001/',
             // url: 'https://mycargonaut.onrender.com/',
             offer: {
-                description: "",
-                price: "",
-                car: "",
+                driverId: 0,
+                vehicleId: 0,
                 start: "",
-                end: "",
+                destination: "",
+                dateTime: "",
+                price: 0,
+                description: "",
             },
             cars: [],
-            defaultCar: "",
+            currentCar: null,
             carNames: [],
+            user: null,
             rules: [v => v.length <= 128 || 'Max 128 characters, everything after will be cut off'],
         };
     },
@@ -115,14 +119,43 @@ export default {
         })
             .then((response) => {
                 this.cars = response.data.vehicleList
-                this.defaultCar = this.cars[0]
+                this.cars.forEach(car => {
+                    const newCar = {
+                        carname: car.brand + " " + car.model,
+                        vehicleId: car.vehicleId,
+                    }
+                    this.carNames.push(newCar)
+                })
+                this.offer.vehicleId = this.carNames[0].vehicleId
             })
         },
         createOffer() {
-            console.log(this.offer);
-            this.cars.forEach(car=> {
-                console.log(car)
+            this.cars.forEach(car => {
+                if (car.vehicleId === this.currentCar.vehicleId) {
+                    this.offer.vehicleId = car.vehicleId
+                }
             })
+            this.offer.dateTime = new Date().toISOString().slice(0, 19).replace('T', ' ');
+            this.offer.driverId = this.user.uId
+            console.log(this.offer)
+            this.axios.post(this.url+'rides', this.offer)
+                .then((response) => {
+                    console.log(response)
+                    this.$router.push('/offers')
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+        checkLogin(){
+        this.axios.get(this.url+'login').then((response) => {
+            if(response.status===200){
+            this.user = response.data
+            }
+        })
+            .catch(function (error) {
+                console.log(error);
+            });
         },
     }
 };
