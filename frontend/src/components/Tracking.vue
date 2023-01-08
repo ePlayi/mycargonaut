d9a19a0fcaf0d4b1e1847880d9de1c53ba722eaf
Maxim <maxim.odenweller@mni.thm.de>
2023-01-08 18:26:16 +0100

    tracking sexy

--- frontend/src/components/Tracking.vue
+++ frontend/src/components/Tracking.vue
@@ -59,7 +59,7 @@
 -->
 
 
-  <div v-if="activeRide===true">
+  <!-- <div v-if="activeRide===true">
     <h1 class="text-center">Lassen Sie diesen Screen aktiv, damit die Position getrackt werden kann</h1>
     <h2>Sie haben folgende Fahrt aktiv:</h2>
     <div>
@@ -67,8 +67,109 @@
     </div>
 
     <button class="btn btn-primary" @click="changeStatusTo(this.activeRideId, 5)">Fahrt abschließen</button>
+  </div> -->
+
+  <div v-if="activeRide===true">
+    <v-container class="tracking-container" align="center" justify="center">
+      <v-card rounded="4">
+        <v-card-title class="mt-4">
+            <h1>Fahrt für "{{activeRides.customerName}}" ist aktiv</h1>
+        </v-card-title>
+        <v-card-text>
+          <h2>Startpunkt: {{activeRides.start}} - Ziel: {{activeRides.destination}}</h2>
+          <div>
+            <p></p>
+          </div>
+          
+          <b style="color: red">Lassen Sie diesen Screen aktiv, damit die Position getrackt werden kann</b>
+        </v-card-text>
+          <v-btn
+          class="my-8"
+            color="red"
+            @click="changeStatusTo(this.activeRideId, 5)"
+            >Fahrt abschließen</v-btn
+          >
+      </v-card>
+    </v-container>
   </div>
-<div v-if="activeRide===false">
+
+  <div v-if="activeRide===false">
+    <v-container class="tracking-container" align="center" justify="center">
+      <v-card v-if="bookings.length===0 && accepted.length===0" rounded="4">
+        <v-card-title>
+          Zur Zeit hast du keine offenen Buchungen oder Fahrten
+        </v-card-title>
+      </v-card>
+
+      <v-card v-if="bookings.length>0" rounded="4">
+        <v-card-title>
+          Wähle eine Fahrt aus, die du tracken möchtest
+        </v-card-title>
+        <v-card-text>
+          <v-select
+            class="ma-8"
+            label="Fahrt auswählen"
+            :items="trackableBookings"
+            :v-model="selectedBooking"
+            :item-text="name"
+            :item-value="name"
+          ></v-select>
+        </v-card-text>
+      </v-card>
+      
+      <v-card rounded="4" class="my-8" v-if="accepted.length>0">
+        <v-card-title>
+          Wähle eine Fahrt aus, die du starten möchtest
+        </v-card-title>
+        <v-card-text>
+          <v-row class="ma-4">
+            <v-col cols="12" sm="9">
+              <v-select
+                v-model="currentAccepted"
+                :items="acceptedNames"
+                item-title="acceptedName"
+                item-value="rideId"
+                label="Fahrt auswählen"
+                return-object
+                single-line
+              ></v-select>
+            </v-col>
+            <v-col cols="12" sm="3">
+              <v-btn
+                color="green"
+                @click="changeStatusTo(currentAccepted.rideId, 4)"
+                >Los gehts!</v-btn
+              >
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </div>
+
+  <div v-if="(activeRide===false && trackableBookings.length>0) || (activeRide===true)">
+    <v-container class="tracking-container">
+      <v-card rounded="4" style="margin: 0; padding: 0">
+        <div class="map-box">
+          <l-map ref="map" v-model:zoom="zoom" :center=position :maxZoom=18>
+            <l-tile-layer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                layer-type="base"
+                name="OpenStreetMap"
+
+            >
+            </l-tile-layer>
+            <l-marker :lat-lng=position>
+
+            </l-marker>
+          </l-map>
+        </div>
+      </v-card>
+    </v-container>
+  </div>
+  
+
+<!-- <div v-if="activeRide===false">
   <h3 class="text-center">Aktiv Trackbare Fahrten</h3>
   <select class="form-select" v-model="activeSelected" @change="getPos()">
     <option v-for="booking in trackableBookings" :key="booking" @click="activeSelected=booking.rideId">{{booking.rideId}} : {{booking.start}}-{{booking.destination}}</option>
@@ -83,21 +184,7 @@
 
 </div>
 
-  <hr>
-  <div class="map-box">
-    <l-map ref="map" v-model:zoom="zoom" :center=position :maxZoom=18>
-      <l-tile-layer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          layer-type="base"
-          name="OpenStreetMap"
-
-      >
-      </l-tile-layer>
-      <l-marker :lat-lng=position>
-
-      </l-marker>
-    </l-map>
-  </div>
+  <hr> -->
 </template>
 
 <script>
@@ -137,7 +224,7 @@ export default {
       // url: 'https://mycargonaut.onrender.com/',
 
       activeSelected:{},
-      selectAccepted:"",
+      selectAccepted:null,
       activeRide: false,
 
       //entry zoomlevel
@@ -150,8 +237,12 @@ export default {
       lat: 0,
 
       bookings:[],
+      bookingNames:[],
+      currentBooking: null,
 
       accepted:[],
+      acceptedNames:[],
+      currentAccepted: null,
 
       activeRides: {},
       activeRideId:0,
@@ -168,6 +259,10 @@ export default {
       })
           .then((response) => {
             this.activeRides=response.data.activeRides
+            if (this.activeRides===undefined) {
+              this.activeRide=false
+              return
+            }
             this.activeRideId = this.activeRides.rideId
             this.activeRide=true
 
@@ -203,6 +298,7 @@ export default {
       })
           .then(()=>{
             //FUCKIT ICH RELOADE DIE PAGE HUSO
+            //^lol
             window.location.reload()
           })
           .catch(function (error) {
@@ -214,7 +310,15 @@ export default {
       })
           .then((response) => {
             this.accepted=response.data.acceptedList
+            this.accepted.forEach(accepted => {
+              const newAccepted = {
+                acceptedName: accepted.customerName + " : " + accepted.start + "-" + accepted.destination,
+                rideId: accepted.rideId,
+              }
+              this.acceptedNames.push(newAccepted)
+            })
             console.log(this.accepted)
+            console.log(this.acceptedNames)
           })
     },
     getOwnPos(){
@@ -250,16 +354,15 @@ export default {
 </script>
 
 <style scoped>
-.tracking-card {
-    max-width: 1300px;
+.tracking-container {
+    max-width: 1000px;
 }
 
 .delivery-icon {
     border: 4px solid black;
 }
 .map-box{
-  height: 30vh;
-  width: 100vw;
-  max-width: 100rem;
+  height: 40vh;
+  max-width: 100%;
 }
 </style>
